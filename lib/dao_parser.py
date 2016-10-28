7269300ef6dc8a1ee15468f0eaa24784a039dbb6
Tiago Lisboa <tiago.caio.ol@gmail.com>
2016-10-28 11:38:12 -0300

    dao insert e update

--- lib/dao_parser.py
+++ lib/dao_parser.py
@@ -1,17 +1,115 @@
 def gen_dao(classes, m):
 	TAB = "   "
 
-	f = open("classesDAO.php", "w")
-	f.write("<?php\n")
+	if m == "s":
+		f = open("classesDAO.php", "w")
+		f.write("<?php\n")
 
 	for classe in classes:
+		if m == "m":
+			f = open(classe+".php", "w")
+			f.write("<?php\n")
+
+
 		f.write("require_once \"" + classe + ".php\";\n")
 		f.write("require_once \"Conexao.php\";\n")
 
-		f.write("")
+		f.write("class " + classe + "DAO {\n")
 
-		# for attr in classes[classe]:
-			# print(TAB + attr)
+		f.write(TAB) # Inicio do insert
+		f.write("public static function insert (" + classe + " $" + classe.lower() + ") { \n")
+
+		attr = classes[classe]
+		# # Remove ID
+		# if attr[len(attr)-1][0] == "&":
+		# 	attr.pop(len(attr)-1)
+
+		f.write(TAB + TAB)
+		f.write("$con = Conexao::connect();\n\n")
+
+		# Pega os atributos do objeto passado
+		for a in attr:
+			if a[0] != "&":
+				f.write(TAB + TAB)
+				f.write("$" + a + " = $" + classe.lower() + "->get" + a.capitalize() + "();\n")
+
+		# Cria o comando sql
+		f.write(TAB + TAB)
+		f.write("$sql = \"INSERT INTO " + classe.lower() + " (")
+		for i,a in enumerate(attr):
+			if a[0] != "&":
+				f.write(a)
+				if i < len(attr)-1 and attr[i+1][0] != "&":
+					f.write(", ")
+		f.write(") VALUES (")
+		for i,a in enumerate(attr):
+			if a[0] != "&":
+				f.write("\'$" + a + "\'")
+				if i < len(attr)-1 and attr[i+1][0] != "&":
+					f.write(", ")
+		f.write(")\";\n\n")
+
+		f.write(TAB + TAB)
+		f.write("$resultado = mysqli_query($con, $sql);\n")
+
+		f.write(TAB + TAB)
+		f.write("if(!$resultado) {echo mysqli_error ($con);} \n\n")
+
+		f.write(TAB + TAB)
+		f.write("Conexao::close();\n")
+		f.write(TAB)
+		f.write("}\n\n") # Fim do insert
+
+##########################################################################
+
+		f.write(TAB) # Inicio do update
+		f.write("public static function update (" + classe + " $" + classe.lower() + ") { \n")
+
+		attr = classes[classe]
+
+		f.write(TAB + TAB)
+		f.write("$con = Conexao::connect();\n\n")
+
+		# Pega os atributos do objeto passado
+		for a in attr:
+			f.write(TAB + TAB)
+			a = a.replace("&", "")
+			f.write("$" + a + " = $" + classe.lower() + "->get" + a.capitalize() + "();\n")
+
+		# Cria o comando sql
+		f.write(TAB + TAB)
+		f.write("$sql = \"UPDATE " + classe.lower() + " SET ")
+		for i,a in enumerate(attr):
+			# a = a.replace("&", "")
+			if (a[0] != "&"):
+				f.write(a + " = \'$" + a + "\'")
+				if i < len(attr)-1 and attr[i+1][0] != "&":
+					f.write(", ")
+			else:
+				a = a.replace("&", "")
+				f.write("WHERE " + a + " = \'$" + a + '\'')
+		f.write("\";\n\n")
+
+		f.write(TAB + TAB)
+		f.write("$resultado = mysqli_query($con, $sql);\n")
+
+		f.write(TAB + TAB)
+		f.write("if(!$resultado) {echo mysqli_error ($con);} \n\n")
+
+		f.write(TAB + TAB)
+		f.write("Conexao::close();\n")
+		f.write(TAB)
+		f.write("}\n\n") # Fim do update
 
 
-	f.write("?>")
+
+		f.write("}\n\n") # Fim da classe
+
+		if m == "m":
+			f.write("?>")
+			f.close()
+
+
+	if m == "s":
+		f.write("?>")
+		f.close()
